b82dea050933a3fc5928556222fd216a4ee41669
Seval Karaman <seval_karaman653@hotmail.com>
2021-05-26 21:33:08 +0300

    hw7

---
+++ CleanBlog/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// const myLogger = (req, res, next) => {
+//     console.log("Middleware log 1");
+//     next();
+// }
+
+// const myLogger2 = (req, res, next) => {
+//     console.log("Middleware log 2");
+//     next()
+// }
+
+//MIDDLEWARES
+app.use(express.static('public'))
+// app.use(myLogger);
+// app.use(myLogger2);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Sunucu port ${port} de başlatıldı`);
+})

4c6b28b2a6a717c124f29104778176718a8c22a7
Seval Karaman <seval_karaman653@hotmail.com>
2021-05-27 14:18:09 +0300

    hw8

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+const { response } = require('express');
 
 const app = express();
+
+
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
 
 // const myLogger = (req, res, next) => {
 //     console.log("Middleware log 1");
@@ -18,10 +25,27 @@ app.use(express.static('public'))
 // app.use(myLogger);
 // app.use(myLogger2);
 
+//ROUTES
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+   // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+   res.render('index');
 
 })
+app.get('/about', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('about');
+ 
+ })
+ app.get('/addpost', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('add_post');
+ 
+ })
+ app.get('/post', (req, res) => {
+    // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('post');
+ 
+ })
 
 const port = 3000;
 
